395bc92afe7a15bccd116835bfbb222b5ac008dd
Sascha Sauermann <sascha.sauermann@tum.de>
2020-04-16 19:18:19 +0200

    Added autopas version of NVE time integration.

---
+++ src/USER-AUTOPAS/fix_nve_autopas.cpp
@@ -0,0 +1,55 @@
+#include "fix_nve_autopas.h"
+#include "atom.h"
+#include "autopas.h"
+#include <autopas/utils/ArrayMath.h>
+
+
+LAMMPS_NS::FixNVEAutoPas::FixNVEAutoPas(LAMMPS *lmp, int narg, char **arg) :
+    FixNVE(lmp, narg, arg) {
+
+}
+
+void LAMMPS_NS::FixNVEAutoPas::initial_integrate(int /*vflag*/) {
+  do_integrate</*initial*/ true>();
+}
+
+void LAMMPS_NS::FixNVEAutoPas::final_integrate() {
+  do_integrate</*initial*/ false>();
+}
+
+template<bool initial>
+void LAMMPS_NS::FixNVEAutoPas::do_integrate() {
+  using autopas::utils::ArrayMath::mulScalar;
+
+  double dtfm;
+
+  // update v and x of atoms in group
+
+  double *rmass = atom->rmass;
+  double *mass = atom->mass;
+  int *type = atom->type;
+  int *mask = atom->mask;
+  auto &autopas = *lmp->autopas->_autopas;
+  // int nlocal = atom->nlocal;
+  // if (igroup == atom->firstgroup) nlocal = atom->nfirst;
+
+#pragma omp parallel default(none) shared(autopas, rmass, mass, type, mask) private(dtfm)
+  for (auto &particle : autopas) {
+    int idx = particle.getID(); // TODO Handle global index to local mapping?
+    if (mask[idx] & groupbit) {
+
+      if (rmass) {
+        dtfm = dtf / rmass[idx];
+      } else {
+        dtfm = dtf / mass[type[idx]];
+      }
+
+      particle.addV(mulScalar(particle.getF(), dtfm));
+
+      if constexpr (initial) {
+        particle.addR(mulScalar(particle.getV(), dtv));
+      }
+    }
+  }
+}
+
---
+++ src/USER-AUTOPAS/fix_nve_autopas.h
@@ -0,0 +1,32 @@
+#ifdef FIX_CLASS
+
+FixStyle(nve/autopas,FixNVEAutoPas)
+
+#else
+
+#ifndef LMP_FIX_NVE_AUTOPAS_H
+#define LMP_FIX_NVE_AUTOPAS_H
+
+#include "fix_nve.h"
+
+namespace LAMMPS_NS {
+
+class FixNVEAutoPas : public FixNVE {
+
+public:
+  FixNVEAutoPas(class LAMMPS *, int, char **);
+
+  ~FixNVEAutoPas() override = default;
+
+  void initial_integrate(int) override;
+
+  void final_integrate() override;
+
+protected:
+  template<bool initial>
+  void do_integrate();
+};
+
+}
+#endif
+#endif
