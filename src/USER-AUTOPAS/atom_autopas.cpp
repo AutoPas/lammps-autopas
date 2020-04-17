15b461dd5e5a42f9c259dcc201ed9f5219f8455e
Sascha Sauermann <sascha.sauermann@tum.de>
2020-04-17 19:38:00 +0200

    Implemented AtomAutoPas

--- src/USER-AUTOPAS/atom_autopas.cpp
+++ src/USER-AUTOPAS/atom_autopas.cpp
@@ -1,4 +1,5 @@
 #include "atom_autopas.h"
+#include "error.h"
 
 using namespace LAMMPS_NS;
 
@@ -8,4 +9,10 @@ AtomAutoPas::AtomAutoPas(class LAMMPS *lmp) : Atom(lmp) {
 
 AtomAutoPas::~AtomAutoPas() {
 
+}
+
+void AtomAutoPas::sort() {
+  sortfreq = 0;
+  error->warning(FLERR,"Atom sorting is not supported using AutoPas "
+                       "and will thus be disabled.");
 }
--- src/USER-AUTOPAS/atom_autopas.h
+++ src/USER-AUTOPAS/atom_autopas.h
@@ -14,7 +14,7 @@ class AtomAutoPas : public Atom {
   //void sync(const ExecutionSpace space, unsigned int mask);
   //void modified(const ExecutionSpace space, unsigned int mask);
   //void sync_overlapping_device(const ExecutionSpace space, unsigned int mask);
-  //void sort() override;
+  void sort() override;
   //virtual void grow(unsigned int mask);
   //int add_custom(const char *, int) override;
   //void remove_custom(int, int) override;
