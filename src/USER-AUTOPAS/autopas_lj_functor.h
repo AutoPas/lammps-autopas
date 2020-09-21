4f2661eae43c25e8d413aee31d3b2bc5ca322a39
FG-TUM <f.gratl@tum.de>
2020-09-21 16:28:26 +0200

    add missing dummy check

--- src/USER-AUTOPAS/autopas_lj_functor.h
+++ src/USER-AUTOPAS/autopas_lj_functor.h
@@ -159,7 +159,7 @@ public:
   }
 
   void AoSFunctor(Particle &i, Particle &j, bool newton3) override {
-    if (!doesInteract(i.getTypeId(), j.getTypeId()))
+    if (!doesInteract(i, j))
       return;
     using namespace autopas;
     auto sigmasquare = _sigmasquare;
@@ -1324,8 +1324,8 @@ private:
     }
   }
 
-  inline int doesInteract(size_t i, size_t j) const {
-    return _interactingTypes[i - 1][j - 1];
+  inline int doesInteract(const Particle &i, const Particle &j) const {
+    return _interactingTypes[i.getTypeId() - 1][j.getTypeId() - 1] and not (i.isDummy() or j.isDummy());
   }
 
   /**
